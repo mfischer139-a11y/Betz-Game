360381f2e06bf529ec1df51dc19b64c1f2b6fe56
mfischer139-a11y <m.fischer13.9@gmail.com>
2026-08-24 21:05:26 +0200

    Update game.js

--- game.js
+++ game.js
@@ -13,6 +13,11 @@ let lucyY = window.innerHeight * 0.16;
 let linaX = window.innerWidth * 0.72;
 let linaY = window.innerHeight * 0.17;
 
+// Fortschritt der Wald-Aufgabe
+let rideSteps = 0;
+let birdsHeard = false;
+let birdsFound = false;
+
 function updatePositions() {
   if (!riding) {
     lucy.style.left = lucyX + "px";
@@ -45,6 +50,12 @@ function move(direction) {
   if (riding) {
     linaX = targetX;
     linaY = targetY;
+
+    // Jeder Reitschritt zählt für die Wald-Mission
+    if (!birdsFound) {
+      rideSteps++;
+      checkForestMission();
+    }
   } else {
     lucyX = targetX;
     lucyY = targetY;
@@ -67,8 +78,10 @@ function checkNearLina() {
   if (distance < 145) {
     actionButton.style.display = "block";
     speech.classList.remove("hidden");
+
     speech.innerHTML =
       "Da ist Lina! 🦄<br>Du kannst jetzt aufsteigen.";
+
     quest.innerHTML =
       "Aufgabe geschafft: Lina gefunden ✨";
   } else {
@@ -88,10 +101,15 @@ function toggleRide() {
     updatePositions();
 
     actionButton.innerHTML = "Absteigen 👧🏼";
-    quest.innerHTML =
-      "Neue Aufgabe: Reite durch den Wald 🌲";
-    speech.innerHTML =
-      "Juhu! Lucy reitet jetzt auf Lina! 🦄✨";
+
+    if (!birdsFound) {
+      quest.innerHTML =
+        "Neue Aufgabe: Reite durch den Wald 🌲";
+
+      speech.innerHTML =
+        "Juhu! Lucy reitet jetzt auf Lina! 🦄✨<br><br>Mal sehen, was wir im Wald entdecken.";
+    }
+
   } else {
     lucyX = linaX - 65;
     lucyY = linaY;
@@ -99,10 +117,59 @@ function toggleRide() {
     updatePositions();
 
     actionButton.innerHTML = "Aufsteigen 🦄";
-    speech.innerHTML = "Lucy ist abgestiegen.";
+
+    if (!birdsFound) {
+      speech.innerHTML = "Lucy ist abgestiegen.";
+    }
 
     checkNearLina();
   }
+}
+
+function checkForestMission() {
+
+  // Nach einigen Reitschritten hört Lucy etwas
+  if (rideSteps >= 6 && !birdsHeard) {
+    birdsHeard = true;
+
+    quest.innerHTML =
+      "Hör genau hin... 🐦";
+
+    speech.innerHTML =
+      "Piep! Piep! 🐦<br><br>Lucy hört Vögel irgendwo im Wald!";
+  }
+
+  // Wenig später findet sie die Vögel
+  if (rideSteps >= 11 && !birdsFound) {
+    birdsFound = true;
+
+    quest.innerHTML =
+      "Vögel gefunden! 🐦✨";
+
+    speech.innerHTML =
+      "Da sind sie! 🐦🐤🐦🐤<br><br>Oh je... die Vögel sind völlig durcheinander!";
+
+    showBirdButton();
+  }
+}
+
+function showBirdButton() {
+  actionButton.style.display = "block";
+  actionButton.innerHTML = "Zu den Vögeln 🐦";
+
+  // Der Button startet jetzt die nächste Mission
+  actionButton.onclick = startBirdMission;
+}
+
+function startBirdMission() {
+
+  quest.innerHTML =
+    "Neue Aufgabe: Hilf den Vögeln 🐦";
+
+  speech.innerHTML =
+    "Die verschiedenen Vögel müssen wieder zu ihren Freunden auf den richtigen Ast.<br><br>✨ Als Nächstes bauen wir hier das Sortierspiel!";
+
+  actionButton.style.display = "none";
 }
 
 updatePositions();
